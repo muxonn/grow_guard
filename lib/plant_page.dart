15dd07a8a175d0173645940719456343eb30a9a8
Jakub Madry <01178327@pw.edu.pl>
2023-10-27 17:58:00 +0200

    Add top navbar and mushroom img in plant page

--- lib/plant_page.dart
+++ lib/plant_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:grow_guard/utils/colors.dart';
 
 class PlantPage extends StatelessWidget {
   const PlantPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: forestGreen,
+      appBar: AppBar(
+        toolbarHeight: 130,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,
+            size: 36,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/pleurotus-eryngii.png"),
+            fit: BoxFit.contain,
+          ),
+        ),
+        margin: EdgeInsets.only(right: 1),
+      ),
+    );
   }
 }
