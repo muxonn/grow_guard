3bc850315c7ceaa18c6a0b10e74d24043c50066a
Jakub Madry <jaiwiecejmnie@gmail.com>
2023-11-05 23:31:21 +0100

    Add alternative logo and resize login block

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,56 +8,74 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: forestGreen,
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(100),
-              bottomRight: Radius.circular(100),
-            ),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                spreadRadius: 10,
-                blurRadius: 10,
-                offset: Offset(0, 0),
-              ),
-            ],
-          ),
-          width: size.width - 50,
-          height: size.height - 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Log in",
-                style: TextStyle(fontSize: 40),
-              ),
-              SizedBox(height: 30),
-              InputBox(text: "Email", icon: Icons.person),
-              SizedBox(height: 30),
-              InputBox(text: "Password", icon: Icons.key),
-              SizedBox(height: 30),
-              Container(
-                width: double.infinity,
-                height: 59,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    backgroundColor: forestGreen,
-                  ),
-                  onPressed: () {},
-                  child: const Text("Login", style: TextStyle(fontSize: 20)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 50),
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                  bottomRight: Radius.circular(100),
                 ),
-              )
-            ],
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 10,
+                    blurRadius: 10,
+                    offset: Offset(0, 0),
+                  ),
+                ],
+              ),
+              width: size.width - 50,
+              height: size.height - 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Log in",
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  SizedBox(height: 40),
+                  InputBox(text: "Email", icon: Icons.person),
+                  SizedBox(height: 30),
+                  InputBox(text: "Password", icon: Icons.key),
+                  SizedBox(height: 30),
+                  Container(
+                    width: double.infinity,
+                    height: 59,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        backgroundColor: forestGreen,
+                      ),
+                      //TODO Add login to firebase
+                      onPressed: () {},
+                      child:
+                          const Text("Login", style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -93,7 +111,7 @@ class InputBox extends StatelessWidget {
           ),
           hintText: text,
           prefixIcon: Padding(
-            padding: const EdgeInsets.only(left: 50, right: 20),
+            padding: const EdgeInsets.only(left: 40, right: 20),
             child: Icon(
               icon,
               color: Colors.black,
